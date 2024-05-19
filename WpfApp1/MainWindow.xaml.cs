851ceed71b224dd1d6e49780cafbe936aae638fa
Sova5 <Sova5@ATROKS>
2024-05-19 15:47:54 +0300

    Добавьте файлы проекта.

---
+++ WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        VarEntities DB = new VarEntities();
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Btn_Click(object sender, RoutedEventArgs e)
+        {
+            User user = new User();
+
+            user.Id= 2;
+            user.Name = TextLogin.Text;
+            user.Password = TextPassword.Text;
+
+            DB.User.Add(user);
+            DB.SaveChanges();
+
+             
+        }
+
+        private void Reg_Click(object sender, RoutedEventArgs e)
+        {
+            new Regml().Show();
+            this.Close();
+
+        }
+    }
+}
---
+++ WpfApp1/Regml.xaml.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Логика взаимодействия для Regml.xaml
+    /// </summary>
+    public partial class Regml : Window
+    {
+        VarEntities DB = new VarEntities();
+        public Regml()
+        {
+            InitializeComponent();
+        }
+
+        public bool Avtr(string Login, string Pass)
+        {
+            var Users = DB.User.ToList();
+            foreach (User User in Users)
+            {
+                if (Login == User.Name & Pass == User.Password)
+                {
+                    return true;
+                }
+
+
+            }
+            return false;
+        }
+        private void Btn_Click(object sender, RoutedEventArgs e)
+        {
+            User user =  new User();
+            user.Id = 1;
+            string Name = "Fe";
+            string Password = "1234";
+            bool res = Avtr(Name ,Password);
+
+            if ( res == true)
+            {
+                new MainWindow().Show();
+                this.Close();
+            }
+
+            //var Users = DB.User.ToList();
+            //foreach (User u in Users )
+            //{
+            //    if (u.Name == user.Name & u.Password == user.Password)
+            //    {
+            //        new MainWindow().Show();
+            //        this.Close();
+            //    }
+                
+            //}
+        }
+    }
+}
